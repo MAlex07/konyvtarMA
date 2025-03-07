2bf2267869d5b59a1ce698e8ebe78fa79d8cb3dd
Petrik Diak <diak@petrik.hu>
2025-03-07 12:30:19 +0100

    EditBookData elkészült

---
+++ konyvtarMA/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvtarMA
+{
+	internal class Book
+	{
+		private string title;
+		private string author;
+		private int pageCount;
+		private int releaseYear;
+
+		public Book(string title, string author, int pageCount, int releaseYear)
+		{
+			this.title = title;
+			this.author = author;
+			this.pageCount = pageCount;
+			this.releaseYear = releaseYear;
+		}
+
+		public string Title { get => title; set => title = value; }
+		public string Author { get => author; set => author = value; }
+		public int PageCount { get => pageCount; set => pageCount = value; }
+		public int ReleaseYear { get => releaseYear; set => releaseYear = value; }
+
+
+		public void GetBookByTitle(Book[] books, string title)
+		{
+
+		}
+
+		public void EditBookData()
+		{
+			this.title = "Top gear";
+			this.author = "Gáspár Győző";
+			this.pageCount = 576;
+			this.releaseYear = 1976;
+		}
+
+
+
+		public override string? ToString()
+		{
+			return $"Cím: {title} / Író: {author} / Oldalai száma: {pageCount} / Megjelenési éve: {releaseYear}";
+		}
+	}
+}
--- konyvtarMA/Program.cs
+++ konyvtarMA/Program.cs
@@ -4,7 +4,19 @@
 	{
 		static void Main(string[] args)
 		{
+			Book konyv1 = new("Egri csillagok", "Gárdonyi Géza", 500, 1901);
+			Book konyv2 = new("Pál utcai fiúk", "Molnár Ferenc", 220, 1906);
+			Book konyv3 = new("A kis herceg", "Antoine de Saint-Exupéry", 96, 1943);
 
-		}
+            Console.WriteLine(konyv1);
+			Console.WriteLine(konyv2);
+			Console.WriteLine(konyv3);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            konyv1.EditBookData();
+			Console.WriteLine(konyv1);
+        }
 	}
 }
